5da6366a207c827985b34a14c2c0e19bf901a2f5
psymoney <sangykimiv@gmail.com>
2021-02-20 18:10:13 +0900

    Amned class and variable names, method structure, and delete unnecessary method

--- Backend/apps/financials/urls.py
+++ Backend/apps/financials/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.CompView.as_view()),
+    path('', views.CompanyLookupView.as_view()),
     #    path('financials/', views.ExternalAPIRequestView.as_view()),
 ]
--- Backend/apps/financials/views.py
+++ Backend/apps/financials/views.py
@@ -6,65 +6,46 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework import status
 
 
-class CompView(APIView):
+class CompanyLookupView(APIView):
     renderer_classes = [JSONRenderer]
-    corporation_name = ""
-    consolidation = ""
 
     def get(self, request):
-        //TODO(SY): uncomment below codes after implementing the get function
-        # corporation_name
-        # corporation_code = get_corporation_code(corporation_name)
-        # financials = get_financials(corporation_code, consolidation)
-        # print(financials)
+        corporate_name = request.query_params["corporate_name"]
+        consolidation_key = request.query_params["consolidation_key"]
+        if consolidation_key == "True":
+            consolidation_key = "CFS"
+        else:
+            consolidation_key = "OFS"
 
-        # get_financial_reports(financials)
+        corporate_code = get_corporate_code(corporate_name)
 
-        return Response(data={"Hello": "world"}, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        name = request.query_params["name"]
-        cons = request.query_params["cons"]
-
-        set_corpName(name)
-        set_fsDiv(cons)
-        corporation_code = CompView.get_corporation_code(name)
-
-        financials = CompView.get_financials(
-            corporation_code, CompView.consolidation)
+        financials = CompanyLookupView.get_financials(
+            corporate_code, consolidation_key)
         for i in range(1, 100):
             print(financials[i])
 
         print(
-            f"corporation_name = {CompView.corporation_name} '\n'consolidation = {CompView.consolidation}")
+            f"corporate_name = {corporate_name} '\n'consolidation_key = {consolidation_key}")
 
-        def set_corpName(name):
-            CompView.corporation_name = name
+        def get_corporate_code(name):
+            # TODO(SY): uncommnet below codes after implementing get_corporate_code method
+            #corpList = request.data()
+            # if name in corpList:
+            return "00126380"
 
-        def set_fsDiv(isCons):
-            if isCons == "True":
-                CompView.consolidation = "CFS"
-            else:
-                CompView.consolidation = "OFS"
+        return Response(data={"corporate_name": corporate_name, "consolidation_key": consolidation_key}, status=status.HTTP_200_OK)
 
-        return Response(data={"corporation_name": CompView.corporation_name, "consolidation": CompView.consolidation}, status=status.HTTP_200_OK)
-
-    def get_financials(corporation_code, consolidation):
+    def get_financials(corporate_code, consolidation_key):
         URI = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json?"
-        crtfcKey = "d3f02b844b4afaa11b10e188bc7a092fc1a63f25"
-        bsnsYear = 2019
-        reportCode = 11011
-        encText = "crtfc_key=" + crtfcKey + "&corp_code=" + corporation_code + \
-            "&bsns_year=" + str(bsnsYear) + "&reprt_code=" + \
-            str(reportCode) + "&fs_div=" + consolidation
-        finanacial_data = urllib.request.Request(URI + encText)
+        API_key = "d3f02b844b4afaa11b10e188bc7a092fc1a63f25"
+        business_year = 2019
+        report_code = 11011
+        encText = "crtfc_key=" + API_key + "&corp_code=" + corporate_code + \
+            "&bsns_year=" + str(business_year) + "&reprt_code=" + \
+            str(report_code) + "&fs_div=" + consolidation_key
+        data_request = URI + encText
+        finanacial_data = data_request.data
         return finanacial_data
-
-    def get_corporation_code(name):
-        //TODO(SY): uncommnet below codes after implementing get_corporation_code method
-        #corpList = request.data()
-        # if name in corpList:
-        return "00126380"
 
     def get_financial_reports(financials):
         return "abc"
