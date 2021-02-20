84307dc11d1bd0dd1f4257ec047e9664edd819d9
psymoney <sangykimiv@gmail.com>
2021-02-20 22:04:05 +0900

    implement get_financials method, add TODOs, and delete completed TODOs

--- Backend/apps/financials/views.py
+++ Backend/apps/financials/views.py
@@ -4,12 +4,40 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework import status
+import requests
 
 
 class CompanyLookupView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
+
+        # TODO(SY): implement get_corporate_code method
+        def get_corporate_code(name):
+            return "00126380"
+
+        def get_financials():
+            URI = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json"
+            API_key = "d3f02b844b4afaa11b10e188bc7a092fc1a63f25"
+            business_year = 2019
+            report_code = 11011
+            API_parameters = {
+                'crtfc_key': API_key,
+                'corp_code': corporate_code,
+                'bsns_year': business_year,
+                'reprt_code': report_code,
+                'fs_div': consolidation_key
+            }
+            # Request financial data from external API
+            data_request = requests.get(URI, params=API_parameters)
+            financial_data = data_request.text
+
+            return financial_data
+
+        # TODO(SY): implement get_financial_reports method
+        def get_financial_reports(financials):
+            return "abc"
+
         corporate_name = request.query_params["corporateName"]
         consolidation_key = request.query_params["consolidationKey"]
         if consolidation_key == "True":
@@ -17,36 +45,12 @@ class CompanyLookupView(APIView):
         else:
             consolidation_key = "OFS"
 
-        corporate_code = get_corporate_code(corporate_name)
-
-        financials = get_financials()
-        for i in range(1, 100):
-            print(financials[i])
-
+        # TODO(SY): delete below print code after test
         print(
-            f"corporate_name = {corporate_name} '\n'consolidation_key = {consolidation_key}")
+            f"corporate_name = {corporate_name} \nconsolidation_key = {consolidation_key}")
 
-        def get_corporate_code(name):
-            # TODO(SY): uncommnet below codes after implementing get_corporate_code method
-            #corpList = request.data()
-            # if name in corpList:
-            return "00126380"
+        corporate_code = get_corporate_code(corporate_name)
+        financials = get_financials()
 
+        # TODO(SY): fix Response after implementing get method
         return Response(data={"corporate_name": corporate_name, "consolidation_key": consolidation_key}, status=status.HTTP_200_OK)
-
-        def get_financials():
-            URI = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json?"
-            API_key = "d3f02b844b4afaa11b10e188bc7a092fc1a63f25"
-            business_year = 2019
-            report_code = 11011
-            parameter_text = "crtfc_key=" + API_key + "&corp_code=" + corporate_code + \
-                "&bsns_year=" + str(business_year) + "&reprt_code=" + \
-                str(report_code) + "&fs_div=" + consolidation_key
-
-            # TODO(SY): request financial data to opendart API
-            data_request = URI + parameter_text
-            financial_data = data_request.data
-            return financial_data
-
-        def get_financial_reports(financials):
-            return "abc"
