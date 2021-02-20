59b809e2bd0ad1535aa81eadf04c77456a451ebe
psymoney <sangykimiv@gmail.com>
2021-02-20 17:18:06 +0900

    Fix commnets, add TODOs

--- Backend/apps/financials/urls.py
+++ Backend/apps/financials/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from . import views
-from .views import *
-from rest_framework.request import Request
 
 urlpatterns = [
     path('', views.CompView.as_view()),
