8409877150645ff234337ba27998260ae7e34665
psymoney <sangykimiv@gmail.com>
2021-03-02 16:53:00 +0900

    note id as primary key

--- Backend/apps/user/models.py
+++ Backend/apps/user/models.py
@@ -5,6 +5,6 @@ class User(models.Model):
     class Meta:
         db_table = "user"
 
-    id = models.AutoField()
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False)
     password = models.CharField(max_length=255, null=False)
