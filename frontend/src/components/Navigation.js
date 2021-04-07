e803a2ba72e71c7db5ac343eda4302736cccbe15
psymoney <sangykimiv@gmail.com>
2021-04-07 17:21:15 +0900

    Add signin/logout switching button

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function Navigation() {
-  return (
-    <div className='navigation'>
-      <Link to="/home">Home</Link>
-      <Link to="/singin">Sign in</Link>
-      <Link to="/signup">Sign up</Link>
-    </div>
-  );
+class Navigation extends React.Component {
+  state = {
+    isLoggedIn: false
+  };
+  render() {
+    return (
+      <div className='navigation'>
+        <Button>
+          <Link to="/home">Home</Link>
+        </Button>
+        <Button color="light">
+        {this.isLoggedIn ?
+        'Log out' : <Link to='/signin'>Sign in</Link>}
+        </Button>
+        <Button>
+          <Link to="/signup">Sign up</Link>
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default Navigation
