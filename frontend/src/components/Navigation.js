1f9645f5ae7e5e6facb69a7cfed28719d03e0af7
psymoney <sangykimiv@gmail.com>
2021-04-10 00:50:04 +0900

    fix navigation component

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-function Navigation() {
-  return (
-    <div className='navigation'>
-      <Link to="/home">Home</Link>
-      <Link to="/singin">Sign in</Link>
-      <Link to="/signup">Sign up</Link>
-    </div>
-  );
+class Navigation extends React.Component {
+  state = {
+    isLoggedIn: false
+  };
+
+  onLogin = () => {
+    this.setState({ isLoggedIn: true });
+  }
+
+  onLogout = () => {
+    this.setState({ isLoggedIn: false });
+
+  }
+
+  render() {
+    let signInButton;
+    let signUpButton;
+    if (this.isLoggedIn) {
+      signInButton = <Button>Log out</Button>;
+      signUpButton = null;
+    } else {
+      signInButton = <Button><Link to='/signin'>Sign in</Link></Button>;
+      signUpButton = <Button><Link to='/signup'>Sign up</Link></Button>;
+    }
+    return (
+      <div className='navigation'>
+        <Button>
+          <Link to="/home">Home</Link>
+        </Button>
+        {signInButton}
+        {signUpButton}
+      </div>
+    );
+  }
 }
 
 export default Navigation
