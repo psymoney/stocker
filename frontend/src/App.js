734265dd1ec25a88f00c80d28c6c50a403495e28
psymoney <sangykimiv@gmail.com>
2021-04-10 00:47:41 +0900

    add sign in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { HashRouter, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import SignIn from './routes/Signin';
 
-function App() {
+function App(props) {
+  console.log(props);
   return (
     <HashRouter>
       <Navigation />
-      <Route />
+      <Route path="/signin" component={SignIn} />
     </HashRouter>
   );
 }
