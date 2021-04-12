eb92af369bf3aaa1572ddc5067024baf41d5ff2d
psymoney <sangykimiv@gmail.com>
2021-04-12 18:28:39 +0900

    fix CORS allowed ports

--- Backend/server/settings.py
+++ Backend/server/settings.py
@@ -55,8 +55,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3080",
-    "http://127.29.208.1:3000"
+    "http://localhost:3000",
+    "http://127.29.208.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 
 ROOT_URLCONF = 'server.urls'
